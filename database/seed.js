f62d8bfbb33b54115349b690ea3a958347a30c9f
Garrett Watson <garrettbauer@gmail.com>
2019-07-10 02:59:06 +0900

    Generates seed data for product names and adds them to the database

--- database/seed.js
+++ database/seed.js
@@ -1,4 +1,5 @@
 const mysql = require('mysql');
+const vocab = require('./seedVocab.js');
 
 const connection = mysql.createPool({
   connectionLimit: 100,
@@ -7,11 +8,32 @@ const connection = mysql.createPool({
   user: 'root',
   password: '',
   database: 'related_products'
-})
+});
+
+const chooseRandomly = (array) => {
+  const index = Math.floor(Math.random() * array.length);
+  return array[index];
+};
+
+const getDescriptors = () => {
+  const capitalize = (word) => {
+    let arr = [...word];
+    arr[0] = arr[0].toUpperCase();
+    return arr.join('');
+  };
+  const first = capitalize(chooseRandomly(vocab.firstDescriptor));
+  const second = capitalize(chooseRandomly(vocab.secondDescriptor));
+  return `${first} ${second}`;
+};
+
+const generateProduct = () => {
+  const productType = `${chooseRandomly(vocab.clothingType)}`;
+  return `${getDescriptors()} ${productType}`;
+};
 
 const seedDb = () => {
   const queryString = 'INSERT INTO products (name) VALUES (?)';
-  connection.query(queryString, 'joy', (err, res) => {
+  connection.query(queryString, generateProduct(), (err, res) => {
     if (err) {
       console.log('error inserting: ', err);
     } else {
@@ -20,4 +42,8 @@ const seedDb = () => {
   });
 };
 
-seedDb();
+let i = 0;
+while (i < 100) {
+  seedDb();
+  i++;
+};
