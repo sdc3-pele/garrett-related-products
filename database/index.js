7149694ad2b6031851920fb0053f34abad235236
Garrett Watson <garrettbauer@gmail.com>
2019-07-12 00:14:09 +0900

    Requires mysqlConnection

--- database/index.js
+++ database/index.js
@@ -1,14 +1,4 @@
-const mysql = require('mysql');
-const config = require('./config.js');
-
-const connection = mysql.createPool({
-  connectionLimit: 100,
-  host: config.port,
-  port: 3306,
-  user: config.user,
-  password: config.password,
-  database: 'related_products',
-});
+const connection = require('./mysqlConnection.js');
 
 const getAllProducts = (callback) => {
   const queryString = 'SELECT * FROM products';
