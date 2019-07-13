f2e5b952a5735092ec2b80925a5435012c964337
Garrett Watson <garrettbauer@gmail.com>
2019-07-14 03:41:51 +0900

    Test chooseRandomly, generateStyle, generateStyleThumbnail, generateProduct

--- database/spec/seed.test.js
+++ database/spec/seed.test.js
@@ -3,3 +3,16 @@ const seed = require('../seed.js');
 test('chooses a random item from array', () => {
   expect(seed.chooseRandomly([1, 2, 3])).not.toBeGreaterThan(3);
 });
+
+test('generates urls', () => {
+  expect(seed.generateStyle()).toMatch(/https/);
+  expect(seed.generateStyleThumbnail()).toMatch(/https/);
+});
+
+test('generates a product object of with correct fields', () => {
+  const productKeys = Object.keys(seed.generateProduct());
+  expect(productKeys.length).toBe(4);
+  productKeys.forEach((key) => {
+    expect(['name', 'price', 'styles', 'styleThumbnails']).toContain(key);
+  });
+});
