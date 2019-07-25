8e1793fca3a8035560e64327fcbeeace11f27a8a
NapoleonDLP <napoleon.dlp@gmail.com>
2019-07-25 00:05:54 -0700

    adjusted to match config file

--- database/mysqlConnection.js
+++ database/mysqlConnection.js
@@ -5,7 +5,7 @@ module.exports = mysql.createPool({
   connectionLimit: 100,
   host: 'localhost',
   port: 3306,
-  user: config.user,
-  password: config.password,
+  user: config.mysql.user,
+  password: config.mysql.password,
   database: 'related_products',
 });
