43627f272aff7f1668214c045d8296d17d2836b3
NapoleonDLP <napoleon.dlp@gmail.com>
2019-08-02 18:04:16 -0700

    completes query functions

--- database/postgres/index.js
+++ database/postgres/index.js
@@ -1,11 +1,28 @@
-// const pool = require('./postgresConnection.js');
-
-/* BUILD ALL OF YOUR QUERY FUNCTIONS HERE */
-
+const { client } = require('./postgresConnection.js');
 // POST FOR A PRODUCT
-
+const postProduct = async (product) => {
+  const postedProduct = await client.query(`INSERT INTO products (name, price, styles, style_thumbnails) VAlUES ('${product.name}', '${product.price}', '${product.styles}', '${product.style_thumbnails}')`);
+  return postedProduct;
+};
 // GET RESPONDS WITH A SPECIFIC PRODUCT
-
+const getProduct = async (id) => {
+  const product = await client.query(`SELECT * FROM products WHERE id = ${id}`);
+  return product;
+};
 // PUT UPDATES A SPECIFIC PRODUCT
-
+const updateProduct = async (id, product) => {
+  const update = await client.query(`UPDATE products SET name = '${product.name}', price = '${product.price}', styles = '${product.styles}', style_thumbnails = '${product.style_thumbnails}' WHERE id = ${id}`);
+  return update;
+};
 // DELETE DELETES A SPECIFIC PRODUCT
+const deleteProduct = async (id) => {
+  const deleted = await client.query(`DELETE FROM products WHERE id = ${id}`);
+  return deleted;
+};
+
+module.exports = {
+  postProduct,
+  getProduct,
+  updateProduct,
+  deleteProduct,
+};
