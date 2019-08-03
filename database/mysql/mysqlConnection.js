737bcc059f05a6070a837527045db396e7d88384
NapoleonDLP <napoleon.dlp@gmail.com>
2019-08-02 18:08:27 -0700

    moves mysql files to mysql folder

---
+++ database/mysql/mysqlConnection.js
@@ -0,0 +1,11 @@
+const mysql = require('mysql');
+const config = require('../config.js');
+
+module.exports = mysql.createPool({
+  connectionLimit: 100,
+  host: 'localhost',
+  port: 3306,
+  user: config.mysql.user,
+  password: config.mysql.password,
+  database: 'related_products',
+});
